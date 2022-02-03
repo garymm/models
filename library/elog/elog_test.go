884fef30722b94f4873b1668451476680a09bab3
Andrew Keenan Richardson <qemqemqem@gmail.com>
2022-02-03 09:58:02 -0800

    Add test to elog

---
+++ library/elog/elog_test.go
@@ -0,0 +1,40 @@
+package elog
+
+import (
+	"github.com/emer/etable/etable"
+	"github.com/emer/etable/etensor"
+	"testing"
+)
+
+func TestScopeKeyStringing(t *testing.T) {
+	sk := GenScopeKey(Train, Epoch)
+	if sk != "Train&Epoch" {
+		t.Errorf("Got unexpected scopekey " + string(sk))
+	}
+	sk2 := GenScopesKey([]EvalModes{Train, Test}, []Times{Epoch, Cycle})
+	if sk2 != "Train|Test&Epoch|Cycle" {
+		t.Errorf("Got unexpected scopekey " + string(sk2))
+	}
+	modes, times := sk2.GetModesAndTimes()
+	if len(modes) != 2 || len(times) != 2 {
+		t.Errorf("Error parsing scopekey")
+	}
+}
+
+func TestItem(t *testing.T) {
+	item := Item{
+		Name: "Testo",
+		Type: etensor.STRING,
+		Compute: ComputeMap{"Train|Test&Epoch|Cycle": func(item *Item, scope ScopeKey, dt *etable.Table, row int) {
+			// DO NOTHING
+		}},
+	}
+	item.ExpandModesAndTimesIfNecessary()
+	_, ok := item.GetComputeFunc(Train, Epoch)
+	if !ok {
+		t.Errorf("Error getting compute function")
+	}
+	if item.HasMode(Validate) || item.HasTimescale(Run) {
+		t.Errorf("Item has mode or time it shouldn't")
+	}
+}
