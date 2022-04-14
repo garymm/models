4ffca3c8f1bb2f44de9893f4d83e365ebf7e3861
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2022-04-14 15:21:40 -0700

    Use parameters found through hyperparameter searching.

--- mechs/ra25/ra25.go
+++ mechs/ra25/ra25.go
@@ -101,24 +101,25 @@ func ConfigParams(ss *sim.Sim) {
 					},
 				},
 			},
+			// Some network parameters chosen by WandB: https://wandb.ai/obelisk/models/reports/Good-Weights-for-RA25--VmlldzoxODMxODI1
 			"Network": &params.Sheet{
 				{Sel: "Layer", Desc: "all defaults",
 					Params: params.Params{
 						// All params with importance >=5 have hypers
 						"Layer.Inhib.Layer.Gi": "1.2", // 1.2 > 1.1     importance: 10
 						// TODO This param should vary with Gi it looks like
-						"Layer.Inhib.ActAvg.Init": "0.04", // 0.04 for 1.2, 0.08 for 1.1  importance: 10
-						"Layer.Inhib.Layer.Bg":    "0.3",  // 0.3 > 0.0   importance: 2
-						"Layer.Act.Decay.Glong":   "0.6",  // 0.6   importance: 2
-						"Layer.Act.Dend.GbarExp":  "0.2",  // 0.2 > 0.1 > 0   importance: 5
-						"Layer.Act.Dend.GbarR":    "3",    // 3 > 2 good for 0.2 -- too low rel to ExpGbar causes fast ini learning, but then unravels importance: 5
-						"Layer.Act.Dt.VmDendTau":  "5",    // 5 > 2.81 here but small effect importance: 1
-						"Layer.Act.Dt.VmSteps":    "2",    // 2 > 3 -- somehow works better importance: 1
-						"Layer.Act.Dt.GeTau":      "5",    // importance: 1
-						"Layer.Act.NMDA.Gbar":     "0.16", //  importance: 7
+						"Layer.Inhib.ActAvg.Init": "0.04",  // 0.04 for 1.2, 0.08 for 1.1  importance: 10
+						"Layer.Inhib.Layer.Bg":    "0.3",   // 0.3 > 0.0   importance: 2
+						"Layer.Act.Decay.Glong":   "0.6",   // 0.6   importance: 2
+						"Layer.Act.Dend.GbarExp":  "0.2",   // 0.2 > 0.1 > 0   importance: 5
+						"Layer.Act.Dend.GbarR":    "3",     // 3 > 2 good for 0.2 -- too low rel to ExpGbar causes fast ini learning, but then unravels importance: 5
+						"Layer.Act.Dt.VmDendTau":  "5",     // 5 > 2.81 here but small effect importance: 1
+						"Layer.Act.Dt.VmSteps":    "2",     // 2 > 3 -- somehow works better importance: 1
+						"Layer.Act.Dt.GeTau":      "5",     // importance: 1
+						"Layer.Act.NMDA.Gbar":     "0.123", //  importance: 7 // From Wandb
 						"Layer.Act.NMDA.MgC":      "1.4",
 						"Layer.Act.NMDA.Voff":     "5",
-						"Layer.Act.GABAB.Gbar":    "0.18", // 0.2 > 0.15  importance: 7
+						"Layer.Act.GABAB.Gbar":    "0.124", // 0.2 > 0.15  importance: 7 // From Wandb
 					}, Hypers: params.Hypers{
 						// These shouldn't be set without also searching for the same value in specific layers like #Input, because it'll clobber them, since it's in a separate Params sheet.
 						//"Layer.Inhib.Layer.Gi":    {"StdDev": "0.15"},
@@ -131,9 +132,9 @@ func ConfigParams(ss *sim.Sim) {
 					}},
 				{Sel: "#Input", Desc: "critical now to specify the activity level",
 					Params: params.Params{
-						"Layer.Inhib.Layer.Gi":    "0.94", // 0.9 > 1.0
-						"Layer.Act.Clamp.Ge":      "1.08", // 1.0 > 0.6 >= 0.7 == 0.5
-						"Layer.Inhib.ActAvg.Init": "0.15", // .24 nominal, lower to give higher excitation
+						"Layer.Inhib.Layer.Gi":    "0.827", // 0.9 > 1.0 // From Wandb
+						"Layer.Act.Clamp.Ge":      "1.26",  // 1.0 > 0.6 >= 0.7 == 0.5 // From Wandb
+						"Layer.Inhib.ActAvg.Init": "0.15",  // .24 nominal, lower to give higher excitation
 					},
 					Hypers: params.Hypers{
 						"Layer.Inhib.Layer.Gi": {"StdDev": ".1", "Min": "0", "Priority": "2", "Scale": "LogLinear"},
@@ -149,9 +150,9 @@ func ConfigParams(ss *sim.Sim) {
 					}},
 				{Sel: "Prjn", Desc: "norm and momentum on works better, but wt bal is not better for smaller nets",
 					Params: params.Params{
-						"Prjn.Learn.Lrate.Base":    "0.08", // 0.04 no rlr, 0.2 rlr; .3, WtSig.Gain = 1 is pretty close  //importance: 10
-						"Prjn.SWt.Adapt.Lrate":     "0.08", // .1 >= .2, but .2 is fast enough for DreamVar .01..  .1 = more minconstraint //importance: 5
-						"Prjn.SWt.Init.SPct":       "0.65", // .5 >= 1 here -- 0.5 more reliable, 1.0 faster..  //importance: 7
+						"Prjn.Learn.Lrate.Base":    "0.2",   // 0.04 no rlr, 0.2 rlr; .3, WtSig.Gain = 1 is pretty close  //importance: 10 // From Wandb
+						"Prjn.SWt.Adapt.Lrate":     "0.08",  // .1 >= .2, but .2 is fast enough for DreamVar .01..  .1 = more minconstraint //importance: 5
+						"Prjn.SWt.Init.SPct":       "0.432", // .5 >= 1 here -- 0.5 more reliable, 1.0 faster..  //importance: 7 // From Wandb
 						"Prjn.Learn.KinaseCa.Rule": "SynSpkTheta",
 					},
 					Hypers: params.Hypers{
